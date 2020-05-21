33233b20359c2965c67c344f31ea0f9cc2a1dace
hibinoy <yuuzin217@gmail.com>
2020-05-21 22:57:50 +0900

    画像アップロードから保存までの処理追加

--- app/Http/Controllers/photoGalleryController.php
+++ app/Http/Controllers/photoGalleryController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\photoGalleryModel;
 use Illuminate\Http\Request;
+use App\Http\Requests\PhotoGalleryRequest;
 
 class PhotoGalleryController extends Controller
 {
@@ -11,7 +13,30 @@ class PhotoGalleryController extends Controller
      *
      * @return Response
      */
-    public function index() {
+    public function index()
+    {
         return view('photoGallery'); // テンプレートに返す
+    }
+
+    /**
+     * 画像の保存
+     *
+     * @return Response
+     */
+    public function store(PhotoGalleryRequest $request)
+    {
+        $photos = $request->photos; // リクエストから画像データを取得
+
+        // 画像をDBに保存
+        for ($i = 0; $i < count($photos); $i++) {
+            $photoName = $request->photos[$i]->getClientOriginalName();        // 画像ファイル名を取得
+            $path = $request->photos[$i]->store('public/photoGallery_images'); // 画像をimageフォルダに保存して、画像パス取得
+            $save = photoGalleryModel::create([                                // DBに値を保存
+                'user_id'       => 1, // IDは仮で指定
+                'original_name' => $photoName,
+                'path'          => $path
+            ]);
+        }
+        return redirect('/')->with('success', '画像をアップロードしました。'); // 成功メッセージ
     }
 }
--- resources/views/photoGallery.blade.php
+++ resources/views/photoGallery.blade.php
@@ -5,12 +5,21 @@
 @endsection
 @section('content')
     <div id="photomain">
+
+        <!-- 成功メッセージ-->
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
         <!-- 画像アップロードフォーム -->
-        <form id="uploadForm" method="POST" action="/photoGallery" enctype="multipart/form-data">
+        <form id="uploadForm" method="POST" action="" enctype="multipart/form-data">
             <input type="file" multiple name="photos[]">
             {{ csrf_field() }}
             <input type="submit" value="アップロード">
         </form>
+
     </div>
 @endsection
 @section('js')
